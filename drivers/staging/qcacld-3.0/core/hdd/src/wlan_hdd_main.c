5f5b35187f0535c2fb15ceeb86cc5cf1e8199849
Ruchit <risenid@duck.com>
2023-10-10 02:24:04 +0530

    qcacld-3.0: Fix a -Werror=strict-prototypes compilation error from clang 15
    Change-Id: Ic49ad37065c73da32651e39ecfee78fd870527e4

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -16850,8 +16850,8 @@ static void hdd_driver_unload(void)
 }
 
 #if defined (SEC_CONFIG_PSM_SYSFS)
-int wlan_hdd_sec_get_psm()
-}
+int wlan_hdd_sec_get_psm(void)
+{
 	int psm = 0;
 
 	psm = cnss_sysfs_get_pm_info();
