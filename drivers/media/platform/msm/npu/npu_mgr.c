03d36bf2d0167847f7711b1cb2ad550277bccafc
Gao Wang <quic_gaowang@quicinc.com>
2024-12-12 16:32:48 +0800

    msm: npu: Fix use after free issue
    There is possibility that network will be used after free.
    This change is to fix this issue.
    
    Change-Id: I2368532df4fc4e75e8d8f2f259a989b5af5f9679
    Signed-off-by: Gao Wang <quic_gaowang@quicinc.com>

--- drivers/media/platform/msm/npu/npu_mgr.c
+++ drivers/media/platform/msm/npu/npu_mgr.c
@@ -1708,6 +1708,13 @@ int32_t npu_host_unload_network(struct npu_client *client,
 		return -EINVAL;
 	}
 
+	if (network->is_executing) {
+		pr_err("network is in execution\n");
+		network_put(network);
+		mutex_unlock(&host_ctx->lock);
+		return -EINVAL;
+	}
+
 	if (network->fw_error) {
 		pr_err("fw in error state, skip unload network in fw\n");
 		goto free_network;
@@ -1969,6 +1976,12 @@ int32_t npu_host_exec_network_v2(struct npu_client *client,
 		goto exec_v2_done;
 	}
 
+	if (network->is_executing) {
+		pr_err("network is already in execution\n");
+		ret = -EINVAL;
+		goto exec_v2_done;
+	}
+
 	if (network->fw_error) {
 		pr_err("fw is in error state\n");
 		ret = -EIO;
@@ -1986,6 +1999,7 @@ int32_t npu_host_exec_network_v2(struct npu_client *client,
 		goto exec_v2_done;
 	}
 
+	network->is_executing = true;
 	for (i = 0; i < num_patch_params; i++) {
 		exec_packet->patch_params[i].id = patch_buf_info[i].buf_id;
 		pr_err("%d: patch_id: %x\n", i,
@@ -2076,6 +2090,7 @@ int32_t npu_host_exec_network_v2(struct npu_client *client,
 
 free_exec_packet:
 	kfree(exec_packet);
+	network->is_executing = false;
 exec_v2_done:
 	network_put(network);
 	mutex_unlock(&host_ctx->lock);
